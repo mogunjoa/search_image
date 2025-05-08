aa5e63787fad8a39bcbfc867fbc292805c88f694
seungohjin <corrobolea@gmail.com>
2025-05-08 20:32:54 +0900

    feat: 에러 핸들링을 위한 runZonedGuarded 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_search/presentation/ui/home/home_screen.dart';
 
 void main() {
-  runApp(
-    ProviderScope(
-      child: HomeScreen(),
-    ),
-  );
+  // Flutter 엔진과 위젯 바인딩 초기화
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runZonedGuarded(() {
+    runApp(
+      HomeScreen(),
+    );
+  }, (error, stackTrace) {
+    // 에러 핸들링
+    debugPrint('에러 발생: $error\n스택 트레이스: $stackTrace');
+  });
 }
